a832b6cc9ca92675b61b495915eee485dc873781
Matheus Almeida <almeida-matheus@hotmail.com.br>
2016-04-07 19:49:25 -0300

    Dots event click

--- app/assets/javascript/full-page-scroll.js
+++ app/assets/javascript/full-page-scroll.js
@@ -37,14 +37,21 @@
 		this.init();
 	};
 
+	/**
+	 * Init plugin
+	 */
 	fullScroll.prototype.init = function () {
 		this.buildSections()
+			.buildDots()
 			.addEvents();
 
 		this.currentPosition = 0;
-		//this.goScroll(this.defaults.container, -50);
 	};
 
+	/**
+	 * Build sections
+	 * @return {Object} this(fullScroll)
+	 */
 	fullScroll.prototype.buildSections = function () {
 		var sections = this.defaults.sections;
 		for (var i = 0; i < sections.length; i++) {
@@ -53,15 +60,49 @@
 		return this;
 	};
 
+	/**
+	 * Build dots navigation
+	 * @return {Object} this (fullScroll)
+	 */
+	fullScroll.prototype.buildDots = function () {
+		var _self = this;
+		var sections = this.defaults.sections;
+		var ul = document.createElement('ul');
+		ul.classList.add('dots');
+
+		var dotsClick = function() {
+			var dataIndex = this.getAttribute('data-index');
+ 			_self.moveScroll(2, null, dataIndex);
+		};
+
+		for (var i = 0; i < sections.length; i++) {
+			var li = document.createElement('li');
+			var a = document.createElement('a');
+			
+			a.addEventListener('click', dotsClick);
+			
+			a.setAttribute('href', '#');
+			a.setAttribute('data-index', i);
+			
+			li.appendChild(a);
+			ul.appendChild(li);
+		}
+
+		document.body.appendChild(ul);
+
+		return this;
+	};
+
+	/**
+	 * Add Events
+	 * @return {Object} this(fullScroll)
+	 */
 	fullScroll.prototype.addEvents = function () {
 		var _self = this;
 		
 		function mousewheel(event){
-			if (event.deltaY > 0) {
- 				_self.moveScroll().down();
-			} else {
-				_self.moveScroll().top();
-			}	
+			var direction = event.deltaY > 0 ? 1 : 0;
+ 			_self.moveScroll(1, direction, null);			
 		}
 		
 		if (document.addEventListener) {
@@ -69,28 +110,24 @@
 		} else {
 			document.attachEvent('mousewheel', mousewheel);
 		}
+		return this;
 	};
 
-	fullScroll.prototype.moveScroll = function (element, position) {
+	/**
+	 * Move scroll
+	 * @param  {Object} Element  Root element
+	 * @param  {Integer} Direction 1 == 'down' 0 == 'top'
+	 * @param  {Integer} Type 1 == 'Top or down' 0 == 'With index'
+	 */
+	fullScroll.prototype.moveScroll = function (type, direction, index) {
 		var _self = this;
-		var publicFunctions = {
-			top: top,
-			down: down
-		};
-		
-		function down () {
-			_self.changePosition(1, 1);
-			_self.animateScroll();
-		}
-
-		function top () {
-			_self.changePosition(1, 0);
-			_self.animateScroll();
-		}
-
-    	return publicFunctions;
+		_self.changePosition(type, direction, index);
+		_self.animateScroll();
 	};	
 
+	/**
+	 * Move scroll with animation
+	 */
 	fullScroll.prototype.animateScroll = function () {
 		var animateTime = this.defaults.animateTime;
         var animateType = this.defaults.animateType;
@@ -106,6 +143,13 @@
 	    this.defaults.container.style.transition = 'all ' + animateTime + 's ' + animateType;
    	};
 
+   	/**
+   	 * Change position of dots, menu and currentPosition variable
+   	 * @param  {[type]} type      [description]
+   	 * @param  {[type]} direction [description]
+   	 * @param  {[type]} dataIndex [description]
+   	 * @return {[type]}           [description]
+   	 */
 	fullScroll.prototype.changePosition = function (type, direction, dataIndex) {
 		var _self = this;
 		if (type == 1) {
