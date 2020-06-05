8af4d598320c18e24bf9f825273d1b19568ba308
amendoa <almeida-matheus@hotmail.com.br>
2020-06-05 10:41:31 -0300

    refactor: update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,37 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp'),
-	browserSync = require('browser-sync'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	cssmin = require('gulp-cssmin');
+var gulp = require("gulp"),
+  browserSync = require("browser-sync"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  cssmin = require("gulp-cssmin");
 
-gulp.task('default', function() {
-	browserSync.init(['app/index.html', 'app/assets/stylesheet/**/*.css', 'app/assets/javascript/**/*.js'], {
-		open: 'external',
-		server: {
-			baseDir: './app/'
-		},
-	});
+gulp.task("default", function () {
+  browserSync.init(
+    [
+      "app/index.html",
+      "app/assets/stylesheet/**/*.css",
+      "app/assets/javascript/**/*.js",
+    ],
+    {
+      open: "external",
+      server: {
+        baseDir: "./app/",
+      },
+    }
+  );
 });
 
-gulp.task('build', function() {	
-	gulp.src('./app/assets/javascript/**/*.js')
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist/javascript/'));	
+gulp.task("build", function () {
+  gulp
+    .src("./app/assets/javascript/**/*.js")
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("./dist/javascript/"));
 
-	gulp.src('./app/assets/stylesheet/**/*.css')
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist/stylesheet/'));
+  gulp
+    .src("./app/assets/stylesheet/**/*.css")
+    .pipe(cssmin())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("./dist/stylesheet/"));
 });
